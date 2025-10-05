3e725f3bdc6f45cec3dac9c016d6d3a124c6a15a
xXNannu09Xx <ngtty2020@gmail.com>
2025-10-05 09:14:37 +0530

    character update

--- scenes/mc/character.gd
+++ scenes/mc/character.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
 
-var walk_SPEED = 150.0
+@export var walk_SPEED = 150.0
+@export var run_SPEED = 300
+@export_range(0, 1) var acceleration = 0.1
+@export_range(0, 1) var deceleration = 0.1
+@export var jump_force = -400
+@export_range(0, 1) var decelerate_on_jump_release = 0.5 
 const JUMP_VELOCITY = -400.0
 
 
@@ -11,13 +16,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force 
+
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
+
+	var speed
+	if  Input.is_action_pressed('dash'):
+		speed = run_SPEED
+	else:
+		speed = walk_SPEED
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * walk_SPEED
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration) 
 	else:
-		velocity.x = move_toward(velocity.x, 0, walk_SPEED)
+		velocity.x = move_toward(velocity.x, 0, walk_SPEED * deceleration)
 
 	move_and_slide()
