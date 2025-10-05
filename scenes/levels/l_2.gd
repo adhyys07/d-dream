840d037e0b7ecbb832ddf572d283010964a718a1
xXNannu09Xx <ngtty2020@gmail.com>
2025-10-05 14:20:23 +0530

    spawner and music

---
+++ scenes/levels/l_2.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var Enemy = preload("res://scenes/characters/enemy1.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_spawn_timer_timeout() -> void:
+	var enemy = Enemy.instantiate()
+	var enemy2 = Enemy.instantiate()
+	add_child(enemy)
+	enemy.position = $Spawn.position
+	add_child(enemy2)
+	enemy.position = $Spawn2.position
